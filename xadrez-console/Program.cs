fabae7192f52dfa353a8685e01b7b0a636c99285
Alisson Kauan <alisson.kauan77@gmail.com>
2022-11-16 15:11:54 -0300

    Melhorando a impementação do metodo de impressão do tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -16,6 +16,7 @@ namespace xadrez_console
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(5, 2));
 
                 Tela.imprimirTabuleiro(tab);
             }
